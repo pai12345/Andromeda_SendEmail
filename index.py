72a911332d5778bd771138f5d38451f815e23da7
Rahul Pai <rahulpai123rockon@gmail.com>
2021-03-04 23:31:37 +0530

    Added Fastapi to send email

--- index.py
+++ index.py
@@ -1,5 +1,22 @@
-import asyncio
+import uvicorn
+from fastapi import FastAPI
+from starlette.middleware.cors import CORSMiddleware
 from Packages.email.email import oServe_email
 
-sendEmail = oServe_email.sendEmail()
-asyncio.run(sendEmail)
+app = FastAPI()
+app.add_middleware(CORSMiddleware,
+                   allow_origins=["*"],
+                   allow_credentials=True,
+                   allow_methods=["*"],
+                   allow_headers=["*"])
+
+
+@app.get("/")
+async def send_email():
+    try:
+        email = await oServe_email.sendEmail()
+        return email
+    except BaseException as error:
+        return f"""{error}"""
+
+uvicorn.run(app, host='0.0.0.0', port=5001)
